d894b203da494b8aeced88b1dc696a71f4282fe6
itjuanr <juanrodriguesdto@gmail.com>
2025-12-29 21:57:08 -0300

    refactor: aplica o hook beforeEach para remover duplicidade de código

--- cypress/e2e/fluxo_cadastro.cy.js
+++ cypress/e2e/fluxo_cadastro.cy.js
@@ -4,36 +4,25 @@ import loginPage from '../support/pages/LoginPage';
 
 describe('Fluxo de Acesso - Automation Exercise', () => {
 
-  it('Deve navegar da Home para a tela de Login corretamente', () => {
-
+  beforeEach(() => {
     homePage.abrirNavegador();
     homePage.validarQueCarregou();
     homePage.irParaLogin();
+  });
 
+  it('Deve navegar da Home para a tela de Login corretamente', () => {
     loginPage.validarPaginaDeLogin();
   });
 
   it('Deve tentar logar com usuário inválido', () => {
-
-    homePage.abrirNavegador();
-    homePage.irParaLogin();
-
     loginPage.preencherLogin('emailerrado@teste.com', 'senha123');
   });
 
   it('Deve tentar cadastrar com usuário inválido', () => {
-
-    homePage.abrirNavegador();
-    homePage.irParaLogin();
-
     loginPage.preencherCadastroErro('teste', 'teste');
   });
 
   it('Deve tentar cadastrar com usuário válido', () => {
-
-    homePage.abrirNavegador();
-    homePage.irParaLogin();
-
     loginPage.preencherCadastroSucesso('teste','teste@teste.com');
   });
 
--- cypress/e2e/fluxo_contato.cy.js
+++ cypress/e2e/fluxo_contato.cy.js
@@ -3,20 +3,17 @@ import contatoPage from '../support/pages/ContatoPage';
 
 describe('Fluxo de Acesso - Automation Exercise', () => {
 
+    beforeEach(() => {
+      homePage.abrirNavegador();
+      homePage.validarQueCarregou();
+      homePage.irParaContato();
+    });
+
   it('Deve navegar da Home para a tela de Contato corretamente', () => {
-
-    homePage.abrirNavegador();
-    homePage.validarQueCarregou();
-    homePage.irParaContato();
-
     contatoPage.validarPaginaDeContato();
   });
 
     it('Deve tentar enviar uma mensagem de contato', () => {
-  
-      homePage.abrirNavegador();
-      homePage.irParaContato();
-
       contatoPage.preencherContato('Teste', 'emailerrado@teste.com', 'Assunto Teste', 'Mensagem Teste', 'teste.txt');
     });
 });
